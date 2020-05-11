f0dc4cc33c6d204a4b718d3779e247047febb94b
LonelyPale <lonelypale@126.com>
2020-05-11 12:21:07 +0800

    update common func

--- goutils.go
+++ goutils.go
@@ -5,6 +5,11 @@ package goutils
 import (
 	"bytes"
 	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // 深拷贝
@@ -14,4 +19,55 @@ func DeepCopy(dst, src interface{}) error {
 		return err
 	}
 	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
+}
+
+// Fmt shorthand, XXX DEPRECATED
+var Fmt = fmt.Sprintf
+
+// TrapSignal catches the SIGTERM and executes cb function. After that it exits
+// with code 1.
+func TrapSignal(cb func()) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		for sig := range c {
+			fmt.Printf("captured %v, exiting...\n", sig)
+			if cb != nil {
+				cb()
+			}
+			os.Exit(1)
+		}
+	}()
+	select {}
+}
+
+func Exit(s string) {
+	fmt.Printf(s + "\n")
+	os.Exit(1)
+}
+
+func EnsureDir(dir string, mode os.FileMode) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.MkdirAll(dir, mode)
+		if err != nil {
+			return fmt.Errorf("Could not create directory %v. %v", dir, err)
+		}
+	}
+	return nil
+}
+
+func FileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return !os.IsNotExist(err)
+}
+
+func WriteFile(filePath string, contents []byte, mode os.FileMode) error {
+	return ioutil.WriteFile(filePath, contents, mode)
+}
+
+func MustWriteFile(filePath string, contents []byte, mode os.FileMode) {
+	err := WriteFile(filePath, contents, mode)
+	if err != nil {
+		Exit(Fmt("MustWriteFile failed: %v", err))
+	}
 }
