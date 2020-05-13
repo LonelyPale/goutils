070b0a6d8ce7f07cb7c6aae56af70a837e0479cb
LonelyPale <lonelypale@126.com>
2020-05-13 23:24:22 +0800

    add sys/dir.go

---
+++ sys/dir.go
@@ -0,0 +1,40 @@
+package sys
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+)
+
+func IsFolderNotExist(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
+}
+
+// DefaultDataDir is the default data directory
+func DefaultDataDir() string {
+	// Try to place the data folder in the user's home dir
+	home := HomeDir()
+	if home == "" {
+		return "./"
+	}
+	switch runtime.GOOS {
+	case "darwin":
+		return filepath.Join(home, "Library", "Application Support")
+	case "windows":
+		return filepath.Join(home, "AppData", "Roaming")
+	default:
+		return home
+	}
+}
+
+func HomeDir() string {
+	if home := os.Getenv("HOME"); home != "" {
+		return home
+	}
+	if usr, err := user.Current(); err == nil {
+		return usr.HomeDir
+	}
+	return ""
+}
