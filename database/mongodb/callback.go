c8f3c51db5a681dd8c9cf06f16d17c66fe050cbd
LonelyPale <lonelypale@126.com>
2020-06-03 11:27:29 +0800

    fix bugs

--- database/mongodb/callback.go
+++ database/mongodb/callback.go
@@ -6,7 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-	"github.com/LonelyPale/goutils/database/mongodb/types"
+	"github.com/LonelyPale/goutils/types"
 )
 
 type BeforeInserter interface {
