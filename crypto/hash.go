a184c09da584a96a45e52b4517794527fe0afdb7
LonelyPale <lonelypale@126.com>
2020-06-06 22:12:51 +0800

    add Hash

---
+++ crypto/hash.go
@@ -0,0 +1,44 @@
+package crypto
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
+type Hash []byte
+
+func NewHash(bs []byte) Hash {
+	return bs
+}
+
+func (h Hash) String() string {
+	return h.Hex()
+}
+
+func (h Hash) Hex() string {
+	return hex.EncodeToString(h)
+}
+
+func (h Hash) Base64() string {
+	return base64.RawURLEncoding.EncodeToString(h)
+}
+
+func (h *Hash) FromHex(s string) error {
+	bs, err := hex.DecodeString(s)
+	if err != nil {
+		return err
+	}
+
+	*h = bs
+	return nil
+}
+
+func (h *Hash) FromBase64(s string) error {
+	bs, err := base64.RawURLEncoding.DecodeString(s)
+	if err != nil {
+		return err
+	}
+
+	*h = bs
+	return nil
+}
