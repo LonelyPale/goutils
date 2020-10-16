ab6a1579b287bfb0a40723009cc3b778e91a13b8
LonelyPale <lonelypale@126.com>
2020-10-17 00:49:42 +0800

    add process、flock

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/uuid v1.1.1
 	github.com/json-iterator/go v1.1.9
+	github.com/mitchellh/go-ps v1.0.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.4.2
--- sys.go
+++ sys.go
@@ -2,11 +2,42 @@ package goutils
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
+
+	"github.com/mitchellh/go-ps"
 )
 
 // exit app
 func Exit(s string) {
 	fmt.Printf(s + "\n")
 	os.Exit(1)
+}
+
+func CreatePIDFile(filepath string) error {
+	if len(filepath) == 0 {
+		var err error
+		if filepath, err = CurrentProcessName(); err != nil {
+			filepath = "./temp.pid"
+		}
+	}
+
+	pid := os.Getpid()
+	data := []byte(strconv.Itoa(pid))
+	if err := ioutil.WriteFile(filepath, data, 0666); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CurrentProcessName() (string, error) {
+	pid := os.Getpid()
+	process, err := ps.FindProcess(pid)
+	if err != nil {
+		return "", err
+	}
+
+	return process.Executable(), nil
 }
