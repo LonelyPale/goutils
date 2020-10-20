34d04d4d08be9b9fb33cfe9547429910392aa520
LonelyPale <lonelypale@126.com>
2020-10-20 14:33:35 +0800

    1、增加注释

--- errors/common_errors.go
+++ errors/common_errors.go
@@ -1,4 +1,4 @@
 package errors
 
-var ErrCanNotNil = New("shouldn't be nil")
-var ErrMustPointer = New("Must be a pointer")
+var ErrCanNotNil = New("shouldn't be nil")    //不能为空
+var ErrMustPointer = New("Must be a pointer") //必须是指针
