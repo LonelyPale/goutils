f98b921249df1629f2068bfafed7ee439a73b021
LonelyPale <lonelypale@126.com>
2021-02-15 17:51:43 +0800

    goutils string update len(s)==0

--- string.go
+++ string.go
@@ -4,16 +4,25 @@ import "unicode"
 
 // 是否首字母大写
 func IsStartUpper(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
 	return unicode.IsUpper([]rune(s)[0])
 }
 
 // 是否首字母小写
 func IsStartLower(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
 	return unicode.IsLower([]rune(s)[0])
 }
 
 // 首字母大写
 func StartUpper(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
 	if IsStartUpper(s) {
 		return s
 	}
@@ -24,6 +33,9 @@ func StartUpper(s string) string {
 
 // 首字母小写
 func StartLower(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
 	if IsStartLower(s) {
 		return s
 	}
