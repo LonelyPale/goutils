bd75aa3990039b164f07686227bd76cc764d76e7
LonelyPale <lonelypale@126.com>
2021-02-16 17:31:05 +0800

    mongodb errors add ErrNoDocuments

--- database/mongodb/errors.go
+++ database/mongodb/errors.go
@@ -1,6 +1,12 @@
 package mongodb
 
-import "github.com/LonelyPale/goutils/errors"
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/LonelyPale/goutils/errors"
+)
+
+var ErrNoDocuments = mongo.ErrNoDocuments
 
 var ErrNilObjectID = errors.New("ObjectID is nil")
 var ErrNilFilter = errors.New("filter cannot be nil")
