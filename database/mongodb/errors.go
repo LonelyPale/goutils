f111bbc6ccf6d6a6716df0e60589f83319e9d247
LonelyPale <lonelypale@126.com>
2021-02-17 18:08:48 +0800

    mongodb add IsErrIndexExists

--- database/mongodb/errors.go
+++ database/mongodb/errors.go
@@ -15,3 +15,14 @@ var ErrNilCollection = errors.New("collection is nil")
 var ErrNilDocument = errors.New("document is nil")
 var ErrDocumentExists = errors.New("document already exists")
 var ErrDocumentNotExist = errors.New("document does not exist")
+
+// 检查是否是触发唯一索引的错误E11000
+func IsErrIndexExists(err error) bool {
+	e, ok := err.(mongo.WriteException)
+	if ok {
+		if len(e.WriteErrors) > 0 && e.WriteErrors[0].Code == 11000 {
+			return true
+		}
+	}
+	return false
+}
