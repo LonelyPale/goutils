d083d50cbb628d3a1883094d4c1156e4eb16b96e
LonelyPale <lonelypale@126.com>
2021-02-22 17:53:06 +0800

    add tls GenerateCert

---
+++ crypto/tls/generate_cert_main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"time"
+)
+
+var (
+	host       = flag.String("host", "", "Comma-separated hostnames and IPs to generate a certificate for")
+	validFrom  = flag.String("start-date", "", "Creation date formatted as Jan 1 15:04:05 2011")
+	validFor   = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
+	isCA       = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
+	rsaBits    = flag.Int("rsa-bits", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
+	ecdsaCurve = flag.String("ecdsa-curve", "", "ECDSA curve to use to generate a key. Valid values are P224, P256 (recommended), P384, P521")
+	ed25519Key = flag.Bool("ed25519", false, "Generate an Ed25519 key")
+)
+
+func main() {
+	flag.Parse()
+	GenerateCert(*host, *validFrom, *validFor, *isCA, *rsaBits, *ecdsaCurve, *ed25519Key)
+}
