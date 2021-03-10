cfa201a81d6c51fd869e5419d431710a90365742
LonelyPale <lonelypale@126.com>
2021-03-10 17:15:56 +0800

    update mongodb starter

--- database/mongodb/starter/starter_mongodb.go
+++ database/mongodb/starter/starter_mongodb.go
@@ -12,14 +12,19 @@ import (
 func init() {
 	SpringBoot.
 		RegisterNameBeanFn("mongodb-client", func(cfg config.MongodbConfig) (*mongodb.Client, error) {
-			SpringLogger.Info("open mongodb client.")
-			return mongodb.Connect(mongodb.NewClientOptionsFromConfig(&cfg))
+			client, err := mongodb.Connect(mongodb.NewClientOptionsFromConfig(&cfg))
+			if err != nil {
+				return nil, err
+			}
+			SpringLogger.Info("Mongodb client opened successfully.")
+			return client, nil
 		}).
 		ConditionOnMissingBean((*mongodb.Client)(nil)).
 		Destroy(func(client *mongodb.Client) {
-			SpringLogger.Info("close mongodb client.")
 			if err := mongodb.CloseClient(client); err != nil {
 				SpringLogger.Error(err)
+				return
 			}
+			SpringLogger.Info("Mongodb client closed successfully.")
 		})
 }
