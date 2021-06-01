579fe0f8e5006a30e29637ef5f28b79405b21ceb
LonelyPale <lonelypale@126.com>
2021-06-01 18:29:18 +0800

    add dir test

---
+++ dir_test.go
@@ -0,0 +1,13 @@
+package goutils
+
+import "testing"
+
+func TestFileExist(t *testing.T) {
+	t.Log(FileExist("/tmp"))
+	t.Log(FileExist("/tmp1"))
+}
+
+func TestFileNotExist(t *testing.T) {
+	t.Log(FileNotExist("/tmp"))
+	t.Log(FileNotExist("/tmp1"))
+}
