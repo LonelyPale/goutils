e25bb2183d4ffcde65ce2a7047fcf2e467fab46a
LonelyPale <lonelypale@126.com>
2021-07-01 11:32:37 +0800

    add session

---
+++ thirdparty/gin/middleware/session.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/memstore"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	SecretKey   = "github.com/LonelyPale/goutils"
+	SessionName = "s"
+)
+
+var store sessions.Store
+
+func Session() gin.HandlerFunc {
+	store = memstore.NewStore([]byte(SecretKey))
+	return sessions.Sessions(SessionName, store)
+}
+
+func GetSession(c *gin.Context) sessions.Session {
+	return sessions.Default(c)
+}
