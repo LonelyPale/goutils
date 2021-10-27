eabe69e64e68b865cbdf6ba9741d03875058d351
LonelyPale <lonelypale@126.com>
2021-10-27 16:10:47 +0800

    temp

--- database/redis/config.go
+++ database/redis/config.go
@@ -2,14 +2,14 @@ package redis
 
 type Config struct {
 	Endpoint string `value:"${redis.endpoint:=localhost:6379}"`
-	Password string `value:"${redis.password:=}"`
+	Password string `value:"${redis.password:=123456}"`
 	Database int    `value:"${redis.database:=0}"`
 }
 
 func DefaultConfig() *Config {
 	return &Config{
 		Endpoint: "localhost:6379",
-		Password: "",
+		Password: "123456",
 		Database: 0,
 	}
 }
